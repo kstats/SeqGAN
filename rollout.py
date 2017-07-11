01c411ab5585f796a6bb19f887a426c89c718008
gil <gillederman@gmail.com>
2017-07-10 19:47:32 -0700

    fixed rewards on top of master

--- rollout.py
+++ rollout.py
@@ -75,30 +75,34 @@ class ROLLOUT(object):
         self.gen_x = tf.transpose(self.gen_x, perm=[1, 0])  # batch_size x seq_length
 
     def get_reward(self, sess, input_x, rollout_num, discriminator):
-        rewards = []
-        for i in range(rollout_num):
-            for given_num in range(1, 20):
-                feed = {self.x: input_x, self.given_num: given_num}
-                samples = sess.run(self.gen_x, feed)
-                feed = {discriminator.input_x: samples, discriminator.dropout_keep_prob: 1.0}
-                ypred_for_auc = sess.run(discriminator.ypred_for_auc, feed)
-                ypred = np.array([item[1] for item in ypred_for_auc])
-                if i == 0:
-                    rewards.append(ypred)
-                else:
-                    rewards[given_num - 1] += ypred
-
-            # the last token reward
-            feed = {discriminator.input_x: input_x, discriminator.dropout_keep_prob: 1.0}
-            ypred_for_auc = sess.run(discriminator.ypred_for_auc, feed)
-            ypred = np.array([item[1] for item in ypred_for_auc])
-            if i == 0:
-                rewards.append(ypred)
-            else:
-                rewards[19] += ypred
-
-        rewards = np.transpose(np.array(rewards)) / (1.0 * rollout_num)  # batch_size x seq_length
+        rewards = np.zeros(shape=(64,20))
+        rewards.fill(2.94443631)
         return rewards
+    # def get_reward(self, sess, input_x, rollout_num, discriminator):
+    #     rewards = []
+    #     for i in range(rollout_num):
+    #         for given_num in range(1, 20):
+    #             feed = {self.x: input_x, self.given_num: given_num}
+    #             samples = sess.run(self.gen_x, feed)
+    #             feed = {discriminator.input_x: samples, discriminator.dropout_keep_prob: 1.0}
+    #             ypred_for_auc = sess.run(discriminator.ypred_for_auc, feed)
+    #             ypred = np.array([item[1] for item in ypred_for_auc])
+    #             if i == 0:
+    #                 rewards.append(ypred)
+    #             else:
+    #                 rewards[given_num - 1] += ypred
+
+    #         # the last token reward
+    #         feed = {discriminator.input_x: input_x, discriminator.dropout_keep_prob: 1.0}
+    #         ypred_for_auc = sess.run(discriminator.ypred_for_auc, feed)
+    #         ypred = np.array([item[1] for item in ypred_for_auc])
+    #         if i == 0:
+    #             rewards.append(ypred)
+    #         else:
+    #             rewards[19] += ypred
+
+    #     rewards = np.transpose(np.array(rewards)) / (1.0 * rollout_num)  # batch_size x seq_length
+    #     return rewards
 
     def create_recurrent_unit(self):
         # Weights and Bias for input and hidden tensor
